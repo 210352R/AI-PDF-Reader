835f02133fe32fb5a1cbe5971559be87cc4258c2
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-06-22 21:05:25 +0530

    first setup part

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from pdfminer.high_level import extract_text
+import pdf_helper
+
+# Set page title and favicon
+st.set_page_config(page_title="Upload PDF", page_icon=":file_folder:")
+
+# Display header
+st.title("Upload your PDF file")
+
+# File size limit in megabytes
+MAX_FILE_SIZE_MB = 200
+# File upload widget
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+# Check if a file is uploaded
+if uploaded_file is not None:
+
+    try:
+        # Check the file size
+        file_size_mb = uploaded_file.size / (1024 * 1024)
+        if file_size_mb > MAX_FILE_SIZE_MB:
+            st.error(
+                f"File size exceeds the limit of {MAX_FILE_SIZE_MB} MB. Please upload a smaller file."
+            )
+        else:
+            # Extract text from the uploaded PDF file
+            text = extract_text(uploaded_file)
+
+            page_count = pdf_helper.extract_pages(uploaded_file)
+
+            st.subheader(f"Pages: {page_count}")
+
+            # Display the extracted text on the web page
+            st.subheader("Extracted Text")
+            st.write(text.strip())
+
+            # Check if the PDF has images
+            uploaded_file.seek(0)  # Reset the file pointer to the beginning
+            has_images = pdf_helper.pdf_has_images(uploaded_file)
+            print(has_images)
+
+    except Exception as e:
+        print(e)
+        st.error(f"An error occurred: {e}")
---
+++ pdf_helper.py
@@ -0,0 +1,24 @@
+from pdfminer.high_level import extract_text, extract_pages
+import fitz
+
+
+def get_pdf_page_count(file_path):
+    pdf_document = fitz.open(file_path)
+    page_count = pdf_document.page_count
+    pdf_document.close()
+    print(page_count)
+    return page_count
+
+
+# Function to check if the PDF contains images
+def pdf_has_images(pdf_file):
+    doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        image_list = page.get_images(full=True)
+        if image_list:
+            return True  # At least one image found
+    return False
+
+
+get_pdf_page_count("example.pdf")
